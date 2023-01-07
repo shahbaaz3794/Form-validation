2554adf28ff57edfbc0816b5f752c5241abdc831
shahbaaz3794 <shahbaaz.ahmad3794@gmail.com>
2023-01-07 14:02:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState, useEffect, useRef } from "react";
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  })
+  const submitted = useRef(false)
+  const [error, setError] = useState({
+    email: false,
+    password: false
+  })
+
+  const handleChange = (key, value) => {
+    setState({ ...state, [key]: value })
+  }
+
+  useEffect(() => {
+    if (submitted.current) {
+      handleValidate()
+    }
+  }, [state])
+
+  const handleValidate = (callback) => {
+    let Obj = {}
+    for (let i in state) {
+      if (state[i] === "") {
+        Obj = { ...Obj, [i]: true }
+      } else {
+        Obj = { ...Obj, [i]: false }
+      }
+    }
+    if (submitted.current) {
+      setError(Obj)
+      if (callback) {
+        callback(Obj)
+      }
+    }
+    return Obj
+  }
+
+  const handleSubmit = async () => {
+    submitted.current = true
+    handleValidate((errObj) => {
+      if (!Object.values(errObj)?.includes(true)) {
+        console.log(state, "submitted")
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box'>
+        <TextField className='input' error={error?.email} id={error?.email ? "outlined-error" : "outlined-basic"} label="Email" variant="outlined" onChange={(e) => handleChange("email", e.target.value)} />
+        <TextField className='input' error={error?.password} id={error?.password ? "outlined-error" : "outlined-basic"} label="Password" variant="outlined" onChange={(e) => handleChange("password", e.target.value)} />
+        <Button onClick={handleSubmit} variant="contained">Login</Button>
+      </div>
     </div>
   );
 }
